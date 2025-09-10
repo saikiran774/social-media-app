b55124c9bd5c735816cdcc1d4c5338badd6fec89
Lavudya saikiran <officialsai@74gmail.com>
2025-09-10 15:26:29 +0530

    Configured Clerk for production

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected'))
-        await mongoose.connect(`${process.env.MONGODB_URI}/pingup`)
+        await mongoose.connect(`${process.env.MONGODB_URI }/pingup`)
     } catch (error) {
         console.log(error.message)
     }
